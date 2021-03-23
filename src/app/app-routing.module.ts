8d0c2ecf4f16c5405f990cb8324153dffe6946e5
LuisMJ1197 <luisfermjua@gmail.com>
2021-03-23 12:22:44 -0600

    Se ha creado el componente menu-header que contiene el header de la app y el menu lateral.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuHeaderComponent } from './components_/menu-header/menu-header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: MenuHeaderComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuHeaderComponent } from './components_/menu-header/menu-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuHeaderComponent
   ],
   imports: [
     BrowserModule,
